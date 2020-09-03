809f8da0673b629d47c06884f69b724f7aa14147
T-Norman <66390259+T-Norman@users.noreply.github.com>
2020-09-03 11:18:45 -0500

    Update README and added comments

--- ISS_Tracker.py
+++ ISS_Tracker.py
@@ -7,29 +7,29 @@ import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 import time
 
-loc = requests.get('http://api.open-notify.org/iss-now.json')
+loc = requests.get('http://api.open-notify.org/iss-now.json')   # Get .json files from NASA public API
 people = requests.get('http://api.open-notify.org/astros.json')
 
-if loc.status_code != 200 or people.status_code != 200:
+if loc.status_code != 200 or people.status_code != 200: # Checks if API is available
     print('Error getting API')
     quit()
 
 
-loc_json = loc.json()
+loc_json = loc.json()       # Convert to from json to Python
 people_json = people.json()
 
 print("There are", people_json['number'], "people in space right now.")
 
-if people_json['number'] > 0:
+if people_json['number'] > 0:   # Display current astronauts
     for i in people_json['people']:
         print(i['name'].ljust(20, " "), " currently onboard", i['craft'])
 
-print("\nThe ISS coordinates as of", datetime.fromtimestamp(loc_json['timestamp']))
+print("\nThe ISS coordinates as of", datetime.fromtimestamp(loc_json['timestamp'])) # Display first read of a data timestamp
 print("LATITUDE: ", loc_json['iss_position']['latitude'], "\nLONGITUDE:", loc_json['iss_position']['longitude'], '\n')
 
-while 1 < 2:
+while 1 < 2: # inf
 
-    ax = plt.axes(projection=ccrs.PlateCarree())
+    ax = plt.axes(projection=ccrs.PlateCarree()) # Plot world map
     ax.stock_img()
 
     loc = requests.get('http://api.open-notify.org/iss-now.json')
@@ -38,7 +38,7 @@ while 1 < 2:
     lat = float(loc_json['iss_position']['latitude'])
     lon = float(loc_json['iss_position']['longitude'])
     
-    plt.plot(lon, lat, color='blue', linewidth=2, marker='o', transform=ccrs.PlateCarree())
+    plt.plot(lon, lat, color='blue', linewidth=2, marker='o', transform=ccrs.PlateCarree())     # Plot location of ISS
     plt.text(lon - 3, lat - 10, 'ISS', horizontalalignment='right', transform=ccrs.Geodetic())
-    plt.pause(5)
-    plt.clf()
+    plt.pause(5)    # Pause 5 seconds
+    plt.clf()       # Clear previous position
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Python_API_Project
- Tracks ISS across the globe
+# ISS_Global_Tracker
+ This project overlays the GPS position of the ISS over a map of the globe
+ (Does not account for altitude)
+ 
+ Requires Cartopy, which can be installed from the miniconda python environment
+ Requires Requests
+ (conda install -c conda-forge cartopy)
+ (pip install requests)
+ 
+ All data is taken directly from NASA's public API.
+ Data is pulled every 5 seconds and auto-updates the ISS position on the map
